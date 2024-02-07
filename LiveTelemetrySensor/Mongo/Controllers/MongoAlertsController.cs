f46f28b8276a3db0b622a327a6b96084aa47ae9f
shalevdev <shalevVarietyDeveloper@gmail.com>
2024-02-07 07:50:04 +0200

    mongo db request

---
+++ LiveTelemetrySensor/Mongo/Controllers/MongoAlertsController.cs
@@ -0,0 +1,31 @@
+﻿using LiveTelemetrySensor.Mongo.Models.Dtos;
+using LiveTelemetrySensor.Mongo.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace LiveTelemetrySensor.Mongo.Controllers
+{
+    [Route("mongo-alerts")]
+    [ApiController]
+    public class MongoAlertsController: Controller
+    {
+        private MongoAlertsService _mongoAlertsService;
+
+        public MongoAlertsController(MongoAlertsService mongoAlertsService)
+        {
+            _mongoAlertsService = mongoAlertsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAlerts([FromQuery]GetAlertsQueryParams queryParams)
+        {
+            return Ok(JsonConvert.SerializeObject(await _mongoAlertsService.GetAlerts(
+                queryParams.MinTimeStamp,
+                queryParams.MaxTimeStamp,
+                queryParams.MaxSamplesInPage,
+                queryParams.PageNumber
+                )));
+        }
+    }
+}
--- LiveTelemetrySensor/Mongo/Services/MongoAlertsService.cs
+++ LiveTelemetrySensor/Mongo/Services/MongoAlertsService.cs
@@ -1,6 +1,7 @@
 ﻿using LiveTelemetrySensor.Mongo.Models;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace LiveTelemetrySensor.Mongo.Services
@@ -9,7 +10,8 @@ namespace LiveTelemetrySensor.Mongo.Services
     {
         private readonly IMongoCollection<Alert> _alertsCollection;
         private const string MONGO_DB_KEY = "MongoDB";
-        
+        private const string TIMESTAMP_MONGO_KEY = "timestamp";
+
         public MongoAlertsService(IConfiguration configuration)
         {
             var databaseSettings = configuration.GetSection(MONGO_DB_KEY).Get<AlertsDatabaseSettings>();
@@ -20,5 +22,23 @@ namespace LiveTelemetrySensor.Mongo.Services
 
         public async Task InsertAlert(Alert alert) =>
             await _alertsCollection.InsertOneAsync(alert);
+
+        public async Task<List<Alert>> GetAlerts(long minTimeSpan, long maxTimeSpan, int maxSamplesInPage, int pageNumber)
+        {
+            var findOptions = new FindOptions<Alert>
+            {
+                Limit = maxSamplesInPage,
+                Skip = (pageNumber) * maxSamplesInPage,
+                //Sort = Builders<Alert>.Sort.Ascending(TIMESTAMP_MONGO_KEY)
+            };
+            FilterDefinition<Alert> filter = Builders<Alert>.Filter.And(
+                Builders<Alert>.Filter.Gt(TIMESTAMP_MONGO_KEY, minTimeSpan),
+                Builders<Alert>.Filter.Lt(TIMESTAMP_MONGO_KEY, maxTimeSpan)
+                );
+            using (IAsyncCursor<Alert> cursor = await _alertsCollection.FindAsync(filter, findOptions))
+            {
+                return cursor.ToList();
+            }
+        }
     }
 }
